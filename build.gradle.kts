77585352c8208e46ab4c4fc661e112999164b94e
Vitor <vitorgpires18@gmail.com>
2025-11-21 17:02:39 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// build.gradle.kts (Arquivo da RAIZ do Projeto)
+
+plugins {
+    id("com.android.application") version "8.3.2" apply  false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    // LINHA ADICIONADA: Declara o plugin de serialização e sua versão
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0" apply false
+
+}
